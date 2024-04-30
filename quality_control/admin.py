18782e41e8c9a430cdf8266ff10d95ca07a74a17
EinfachSasha <alex_deley@mail.ru>
2024-04-30 19:55:12 +0300

    Complete up to lesson 17

--- quality_control/admin.py
+++ quality_control/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import BugReport, FeatureRequest
 
-# Register your models here.
+def set_status_new(modeladmin, request, queryset):
+    queryset.update(status='New')
+set_status_new.short_description = "Mark selected reports as New"
+class BugReportAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status', 'priority', 'created_at', 'updated_at']
+    list_filter = ['status', 'priority']
+    search_fields = ['title', 'description']
+    actions = [set_status_new]  # Добавление пользовательского действия
+
+    # Настройка формы редактирования
+    fieldsets = (
+        (None, {'fields': ('title', 'description')}),
+        ('Advanced options', {
+            'fields': ('status', 'priority', 'project', 'task'),
+            'classes': ('collapse',)
+        }),
+    )
+
+class FeatureRequestAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status', 'priority', 'created_at', 'updated_at']
+    list_filter = ['status', 'priority']
+    search_fields = ['title', 'description']
+
+    # Настройка формы редактирования
+    fieldsets = (
+        (None, {'fields': ('title', 'description')}),
+        ('Advanced options', {
+            'fields': ('status', 'priority', 'project', 'task'),
+            'classes': ('collapse',)
+        }),
+    )
+
+# Регистрация моделей
+admin.site.register(BugReport, BugReportAdmin)
+admin.site.register(FeatureRequest, FeatureRequestAdmin)
